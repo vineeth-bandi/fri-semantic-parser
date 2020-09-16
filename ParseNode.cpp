83db659f62c49b8ba0cc63c35bb84ada4ec44a23
Nalin Mahajan <nalinmahajan@outlook.com>
2020-09-16 11:54:36 -0500

    initial setup of parseNode

--- ParseNode.cpp
+++ ParseNode.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include <vector>
+#include "ParseNode.h"
 class ParseNode
 {
+    private:
+    ParseNode* parent_ = nullptr;
+    ParseNode* node_ = nullptr;
+    std::vector<ParseNode*> children_;
+    ParseNode::ParseNode(ParseNode* parent, ParseNode* node, std::vector<ParseNode*> children) : parent_(parent), node_(node), children_(children)  {
+
+    }
+    std::vector<ParseNode*> get_leaves(){
+        if (children_.size() != 0){
+            std::vector<ParseNode*> leaves = children_[0]->get_leaves();
+            std::vector<ParseNode*> leaves2 = children_[1]->get_leaves();
+            leaves.insert(leaves.end(), leaves2.begin(), leaves2.end());
+            return leaves;
+        }
+        std::vector<ParseNode*> leaves;
+        leaves.push_back(this);
+        return leaves;
+    }
 
 };
--- ParseNode.h
+++ ParseNode.h
@@ -1,1 +1,13 @@
+#ifndef PARSENODE_H
+#define PARSENODE_H
 #include "ParseNode.h"
+class ParseNode {
+    private:
+    ParseNode* parent_ = 0;
+    ParseNode* node_ = 0;
+    std::vector<ParseNode*> children_ = 0;
+    virtual ParseNode::ParseNode() = 0
+    virtual std::vector<ParseNode> get_leaves() = 0;
+};
+
+#endif
