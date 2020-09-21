15327945b3f295ecb8851b9e2542e5c213bb0805
Nalin Mahajan <nalinmahajan@outlook.com>
2020-09-21 09:04:29 -0500

    cleaned up header

--- ParseNode.h
+++ ParseNode.h
@@ -9,7 +9,9 @@ private:
     std::vector<ParseNode *> children_ = 0;
     std::vector<int> semantic_form_ = 0;
     std::vector<int> surface_form_ = 0;
-    virtual ParseNode::ParseNode() = 0 virtual std::vector<ParseNode> get_leaves() = 0;
+
+public:
+    virtual std::vector<ParseNode> get_leaves() = 0;
 };
 
 #endif
