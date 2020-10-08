5af80750b5ccde6272bd9d5d3ceb63e85b6e0fa9
Andrew Wu <andrew.m.wu@gmail.com>
2020-10-08 16:26:35 -0500

    .h and .cpp files
    NEED TO FIGURE OUT BOOSTS SO EVERYTHING GETS DONE LOL

--- Lexicon.cpp
+++ Lexicon.cpp
@@ -1,1 +1,77 @@
 #include "Lexicon.h"
+using namespace std;
+Lexicon::Lexicon(Ontology* ontology, , bool expanding_ont){
+	this.ontology = ontology;
+	this.allow_expanding_ont = expanding_ont;
+	generator_should_flush = false;
+	// read lex from files, line 11, how do we do this?
+	// update support structures, do in constructor
+
+}
+
+void Lexicon::compute_pred_to_surface(vector<int> pts){
+
+}
+
+vector<int> Lexicon::compute_reverse_entries(){
+
+}
+
+int Lexicon::calc_exp_args(int idx){
+
+}
+
+int Lexicon::calc_return_cat(int idx){
+
+}
+
+int Lexicon::get_or_add_category(int c){
+	
+}
+
+// two diff returns, can be a str or int
+string Lexicon::compose_str_from_category(int idx){
+
+}
+
+vector<> Lexicon::get_semantic_forms_for_surface_form(vector<>surface_form){
+
+}
+
+vector<> Lexicon::get_surface_forms_for_predicate(vector<> pred){
+
+}
+
+// three diff returns
+void Lexicon::read_lex_from_file(fname, bool allow_expanding_ont, ){
+
+}
+
+void Lexicon::expand_lex_from_strs(vector<> lines, vector<> surface_forms, vector<>semantic_forms, vector<> entries, vector<> pred_to_surface, bool allow_expanding_ont){
+
+}
+
+// two returns, do we want to pass inpointers?
+int Lexicon::read_syn_sem(vector<> s, bool allow_expanding_ont){
+
+}
+
+vector<int> Lexicon::get_all_preds_from_semantic_form(SemanticNode* node){
+
+}
+
+bool Lexicon::form_contains_DESC_predicate(SemanticNode* node){
+
+}
+
+int Lexicon::read_category_from_str(vector<>s){
+
+}
+
+SemanticNode* Lexicon::read_semantic_form_from_str(vector<> s, int category, parent, scoped_lambdas, bool allow_expanding_ont){
+
+}
+
+void Lexicon::delete_semantic_form_for_surface_form(surface_form, int ont_idx){
+
+}
--- Lexicon.h
+++ Lexicon.h
@@ -1,4 +1,51 @@
-class Lexicon
-{
+#include "SemanticNode.h"
+#include <vector>
+#include "Ontology.h"
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+class Lexicon{
+
+// TODO: all vectors change to boosts, confirm types in python and do type checking with boosts
+private:
+  // is this the right way to define an ontology?
+  Ontology* ontology;
+  // what is the type of category -> has lists of ints, and strings
+  vector<int> categories;
+  // look at line 11 for more details on how to implement
+  vector<> entries;
+  vector<> pred_to_surface;
+  vector<> surface_forms;
+  vector<> semantic_forms;
+  vector<> reverse_entries;
+  vector<> sem_form_expected_args;
+  vector<> sem_form_return_cat;
+  vector<> category_consumes;
+  bool generator_should_flush;
+  bool sem_form_expected;
+  bool allow_expanding_ont;
+public:
+	Lexicon(Ontology ontology, , bool expanding_ont)
+	void compute_pred_to_surface(vector<int> pts);
+	vector<int> compute_reverse_entries();
+	int calc_exp_args(int idx);
+	int calc_return_cat(int idx);
+	int get_or_add_category(int c);
+	// two diff returns, can be a str or int
+	string compose_str_from_category(int idx);
+	vector<> get_semantic_forms_for_surface_form(vector<>surface_form);
+	vector<> get_surface_forms_for_predicate(vector<> pred);
+	// three diff returns
+	// do we want to pass in pointers?
+	void read_lex_from_file(fname, bool allow_expanding_ont, );
+	void expand_lex_from_strs(vector<> lines, vector<> surface_forms, vector<>semantic_forms, vector<> entries, vector<> pred_to_surface, bool allow_expanding_ont);
+	// two returns, do we want to pass inpointers?
+	int read_syn_sem(vector<> s, bool allow_expanding_ont);
+	vector<int> get_all_preds_from_semantic_form(SemanticNode* node);
+	bool form_contains_DESC_predicate(SemanticNode* node);
+	int read_category_from_str(vector<>s);
+	SemanticNode* read_semantic_form_from_str(vector<> s, int category, parent, scoped_lambdas, bool allow_expanding_ont)
+	void delete_semantic_form_for_surface_form(surface_form, int ont_idx);
 };
