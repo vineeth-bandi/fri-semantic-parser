fae79812914aee2ac55af67682bbcffd23723747
Nalin Mahajan <nalinmahajan@outlook.com>
2020-10-26 17:49:29 -0500

    fixed parameter types

--- ParseNode.cpp
+++ ParseNode.cpp
@@ -1,15 +1,15 @@
 
 #include "ParseNode.h"
 
-ParseNode::ParseNode(ParseNode *parent, ParseNode *node, std::vector<ParseNode *> children, std::vector<int> surface_form, std::vector<int> semantic_form) : parent_(parent), node_(node), children_(children), surface_form_(surface_form), semantic_form_(surface_form) {
+ParseNode::ParseNode(ParseNode *parent, SemanticNode *node, std::vector<ParseNode *> children, std::string surface_form, std::vector<int> semantic_form) : parent_(parent), node_(node), children_(children), surface_form_(surface_form), semantic_form_(surface_form) {
 
 }
 
 std::vector<ParseNode *> ParseNode::get_leaves() {
     std::vector<ParseNode *> leaves;
-    if (this->children_.size() != 0) {
-        leaves = this->children_[0]->get_leaves();
-        std::vector<ParseNode *> leaves2 = this->children_[1]->get_leaves();
+    if (children_.size() != 0) {
+        leaves = children_[0]->get_leaves();
+        std::vector<ParseNode *> leaves2 = children_[1]->get_leaves();
         leaves.insert(leaves.end(), leaves2.begin(), leaves2.end());
         return leaves;
     }
--- ParseNode.h
+++ ParseNode.h
@@ -2,15 +2,16 @@
 #define PARSENODE_H
 #include <iostream>
 #include <vector>
+#include <SemanticNode.h>
 class ParseNode
 {
 public:
     ParseNode *parent_;
-    ParseNode *node_;
+    SemanticNode *node_;
     std::vector<ParseNode *> children_;
     std::vector<int> semantic_form_;
-    std::vector<int> surface_form_;
-    ParseNode(ParseNode *parent, ParseNode *node, std::vector<ParseNode *> children = {}, std::vector<int> surface_form = {}, std::vector<int> semantic_form = {});
+    std::string surface_form_;
+    ParseNode(ParseNode *parent,  SemanticNode* node, std::vector<ParseNode *> children = {}, std::string surface_form = "", std::vector<int> semantic_form = {});
     std::vector<ParseNode *> get_leaves();
 };
 
